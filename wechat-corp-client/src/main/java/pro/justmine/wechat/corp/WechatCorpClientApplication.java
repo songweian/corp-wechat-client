fede7d9e85e6b34920e01bf0c53185ed05f0275b
weian404 <weian404@gmail.com>
2023-09-29 21:34:38 -0500

    Initial commit

---
+++ wechat-corp-client/src/main/java/pro/justmine/wechat/corp/WechatCorpClientApplication.java
@@ -0,0 +1,13 @@
+package pro.justmine.wechat.corp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WechatCorpClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WechatCorpClientApplication.class, args);
+    }
+
+}
---
+++ wechat-corp-client/src/test/java/pro/justmine/wechat/corp/WechatCorpClientApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.justmine.wechat.corp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WechatCorpClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
